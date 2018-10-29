75464c54a9538f345437d97b8aa46d2b842f0300
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-10-29 02:31:35 -0400

    removeDups adds some comments and a TODO Question

--- Linked_Lists/removeDups/solution.js
+++ Linked_Lists/removeDups/solution.js
@@ -26,6 +26,8 @@ class LinkedList {
     current.next = newNode.head;
   }
 
+  // Boolean checker method used in removeDups:
+  // If it returns true, then two matching node values have been found.
   checkDups(node) {
     let current = this.head;
     while (current !== node) {
@@ -37,9 +39,12 @@ class LinkedList {
     return false;
   }
 
+  // TODO: Question: Why does current.next = current.next.next reassign the
+  // actual nodes whereas current = current.next only serves as a reference?
   removeDups() {
     let current = this.head;
     while (current !== null) {
+      // current.next essentially deleted by being reassigned by its child node
       if (current.next !== null && this.checkDups(current.next)) {
         current.next = current.next.next;
       } else {
