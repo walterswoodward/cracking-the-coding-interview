9451e147a79acf33a5c14d6496ce33810c51524d
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-10-29 15:52:25 -0400

    kthToLast formats js file

--- Linked_Lists/kthToLast/non_recursive.js
+++ Linked_Lists/kthToLast/non_recursive.js
@@ -5,48 +5,47 @@ class LinkedList {
       value, // short hand for value: value
       next: null
     };
-    this._length = 1
+    this._length = 1;
   }
 
   insert(value) {
     let current = this.head;
     let newNode = new LinkedList(value);
-    console.log(this.head)
+    console.log(this.head);
     if (current === undefined) {
       this.head = newNode.head;
-      this._length++
+      this._length++;
     }
     while (current.next) {
       current = current.next;
     }
     current.next = newNode.head;
-    this._length++
+    this._length++;
   }
   // Finds kth value where k = 1 returns the last node, k=2 returns the second to last node etc.
   findNode(k) {
     let current = this.head;
     // console.log(current)
     let counter = this._length - k;
-    if (k < 0 || k >= this._length){
-      throw new Error("No nodes at given position")
+    if (k < 0 || k >= this._length) {
+      throw new Error("No nodes at given position");
     } else {
-      while (counter !== 0){
-        console.log(counter)
-        --counter
-        current = current.next
+      while (counter !== 0) {
+        console.log(counter);
+        --counter;
+        current = current.next;
       }
-      
     }
-    if (current === undefined){
-      throw new Error("No nodes at given position")
+    if (current === undefined) {
+      throw new Error("No nodes at given position");
     }
-    return current
+    return current;
   }
 }
 
 const keyCombinationSeq = new LinkedList(1493827);
 keyCombinationSeq.insert(27453896);
-keyCombinationSeq.insert(19673520)
-keyCombinationSeq.insert(83649956)
-console.log(keyCombinationSeq.findNode(2))
+keyCombinationSeq.insert(19673520);
+keyCombinationSeq.insert(83649956);
+console.log(keyCombinationSeq.findNode(2));
 console.log(keyCombinationSeq);
